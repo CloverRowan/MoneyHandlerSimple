96a73aabc1c7d872a5cf632245da95e3240a5b72
DESKTOP-ITVP7RT\rowan <rowan.clover@gmail.com>
2024-02-10 17:14:14 -0800

    abstracted user query and math to class QueryUser.

---
+++ src/main/java/MoneyHandling/classes/QueryUser.java
@@ -0,0 +1,45 @@
+package MoneyHandling.classes;
+
+import java.util.Scanner;
+
+public class QueryUser
+{
+    public void qUser() {
+        UserInputHandling input1 = new UserInputHandling();
+        Scanner userInputMain = new Scanner(System.in);
+
+        System.out.println("Welcome User, please select the type of currency you'd like to deposit.\n Options: Checks, Cash, Coins, Close.\n");
+
+        String monType = userInputMain.nextLine();
+
+
+        switch (monType.toLowerCase()) {
+            case "checks":
+                input1.setCheckValue();
+                input1.setCheckAmount();
+
+            case "cash":
+                input1.setCashValue();
+                input1.setCashAmount();
+
+            case "coins":
+                input1.setCoinValue();
+                input1.setCoinAmount();
+
+            case "total":
+                double total = (input1.getCashValue() * input1.getCashAmount()) + (input1.getCheckValue() * input1.getCheckCount()) + (input1.getCoinValue() * input1.getCoinAmount());
+                System.out.println("Total Amount deposited is " + total + "$");
+                break;
+
+            case "close":
+                System.out.println("Program closing..\n Thank you, Have a good day.\n");
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Term not recognized,try again.\n");
+                break;
+        }
+    }
+
+}
--- src/main/java/MoneyHandling/classes/UserInputHandling.java
+++ src/main/java/MoneyHandling/classes/UserInputHandling.java
@@ -6,48 +6,78 @@ public class UserInputHandling
 {
     public Scanner userIn = new Scanner(System.in);
     private double checkValue;
-    private double checkCount;
+    private double checkAmount;
     private double cashValue;
     private double cashAmount;
     private double coinValue;
     private double coinAmount;
 
+    public double inputCheck() {
+        String temp = userIn.nextLine();
+        double doubleConvert = 0;
+        try {
+                doubleConvert = Double.parseDouble(temp);
+                return doubleConvert;
+            }  catch(Exception e)
+                {
+                System.out.println("An error has occured, numerical value required.");
+                QueryUser reRun = new QueryUser();
+                reRun.qUser();
+                }
+
+       return doubleConvert;
+    }
 
     public void setCheckValue()
     {
         System.out.println("Please enter the value of the check you'd like to deposit.\n");
-        checkValue = userIn.nextDouble();
+        checkValue = inputCheck();
     }
+
     public void setCheckAmount()
     {
         System.out.println("Please enter the amount of "+ getCheckValue() + "$" +" checks you'd would like to deposit.\n");
-        checkCount = userIn.nextDouble();
+        checkAmount = inputCheck();
     }
+
     public void setCashValue()
     {
         System.out.println("Please enter the value of the Cash bills you'd would like to deposit.\n");
-        cashValue = userIn.nextDouble();
+        //String temp =  userIn.nextLine();
+        cashValue = inputCheck();
     }
+
     public void setCashAmount()
     {
         System.out.println("Please enter the amount of " + getCashValue() + "$" + " bills you'd would like to deposit.\n");
-        cashAmount = userIn.nextDouble();
+        //String temp =  userIn.nextLine();
+        cashAmount = inputCheck();
     }
+
     public void setCoinValue()
     {
         System.out.println("Please enter the value of any coins you'd would like to deposit.\n");
-        coinValue = userIn.nextDouble();
+        //String temp = userIn.nextLine();
+        coinValue = inputCheck();
     }
+
     public void setCoinAmount()
     {
         System.out.println("Please enter the amount of " + getCoinValue() + "c" + " coins you'd would like to deposit.\n");
-        coinAmount = userIn.nextDouble();
+        //String temp = userIn.nextLine();
+        coinAmount = inputCheck();
     }
+
     public double getCheckValue() {return checkValue;}
-    public double getCheckCount() {return checkCount;}
+
+    public double getCheckCount() {return checkAmount;}
+
     public double getCashValue(){return cashValue;}
+
     public double getCashAmount(){return cashAmount;}
+
     public double getCoinValue(){return coinValue;}
+
     public double getCoinAmount(){return coinAmount;}
 
 
